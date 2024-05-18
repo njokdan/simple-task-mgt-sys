4b69227c07724d2ab147fc8f6b5d82d94c323d25
Daniel <njokdan@gmail.com>
2024-05-18 18:12:49 +0000

    update made

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -7,28 +7,28 @@ const auth = (req, res, next) => {
     return res.status(401).json({ error: 'Access denied. No token provided.' });
   }
 
-  try {
-    console.log("process.env.JWT_SECRET", process.env.JWT_SECRET);
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log("decoded", decoded);
-    console.log("decoded_user_id", decoded.userId);
-    //req.user = decoded;
-    req.user = { userId: decoded.userId };
-    next();
-  } catch (err) {
-    res.status(400).json({ error: 'Invalid token.' });
-  }
-
   // try {
   //   console.log("process.env.JWT_SECRET", process.env.JWT_SECRET);
-  //   const decoded = jwt.verify(token.replace('Bearer ', ''), process.env.JWT_SECRET);
+  //   const decoded = jwt.verify(token, process.env.JWT_SECRET);
   //   console.log("decoded", decoded);
   //   console.log("decoded_user_id", decoded.userId);
+  //   //req.user = decoded;
   //   req.user = { userId: decoded.userId };
   //   next();
   // } catch (err) {
   //   res.status(400).json({ error: 'Invalid token.' });
   // }
+
+  try {
+    console.log("process.env.JWT_SECRET", process.env.JWT_SECRET);
+    const decoded = jwt.verify(token.replace('Bearer ', ''), process.env.JWT_SECRET);
+    console.log("decoded", decoded);
+    console.log("decoded_user_id", decoded.userId);
+    req.user = { userId: decoded.userId };
+    next();
+  } catch (err) {
+    res.status(400).json({ error: 'Invalid token.' });
+  }
 };
 
 module.exports = auth;
